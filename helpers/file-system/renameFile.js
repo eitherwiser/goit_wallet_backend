a076158ede2d05ba71ba8636e88c89919aa204ca
Andrew Yatsutko <49717034+eitherwiser@users.noreply.github.com>
2022-02-12 17:51:13 +0200

    Revert "User test"

--- helpers/file-system/renameFile.js
+++
@@ -1,7 +0,0 @@
-const renameFile = async (oldName, newName) => {
-  const [extension] = oldName.split('.').reverse();
-  const renamed = `${newName}.${extension}`;
-  return renamed;
-};
-
-module.exports = renameFile;
--- middlewares/index.js
+++
@@ -1,9 +0,0 @@
-const joiUserValidation = require('./validation/userValidation.js');
-const authenticate = require('./authorization/authorization.js');
-const upload = require('./upload/uploadFile.js');
-
-module.exports = {
-  joiUserValidation,
-  authenticate,
-  upload,
-};
