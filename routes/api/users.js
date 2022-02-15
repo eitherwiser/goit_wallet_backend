7d0a50e0afc658fe2cf69608878a4d518383269d
Andrew Yatsutko <eitherwiser@gmail.com>
2022-02-15 17:16:53 +0200

    added users path '/current', addedt schema to swagger

--- routes/api/users.js
+++ routes/api/users.js
@@ -100,6 +100,30 @@ router.post("/login", async (req, res, next) => {
   }
 });
 
+//current user
+router.get("/current", authenticate, async (req, res, next) => {
+  try {
+    const { _id } = req.user;
+    console.log(_id);
+    const user = await User.findById(_id);
+
+    res.status(200).json({
+      message: `User data '${new Date()}'`,
+      data: {
+        token: user.token,
+        email: user.email,
+        userName: user.userName,
+        avatar: user.avatarURL,
+        balance: user.balance,
+        transactionCategories: user.transactionCategories,
+      },
+    });
+  } catch (error) {
+    console.log(error.message);
+    next(error);
+  }
+});
+
 // user logout
 router.get("/logout", authenticate, async (req, res, next) => {
   try {
